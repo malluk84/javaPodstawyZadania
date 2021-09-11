37833a613731c30f42dca3d24d8eb02bfb624123
Lukasz Malachowski <malachowski.lukasz@gmail.com>
2021-09-11 15:20:16 +0200

    commit

--- src/Task8.java
+++ src/Task8.java
@@ -1,16 +1,21 @@
 public class Task8 {
     public static void main(String[] args) {
-        int x = 2;
-        int y = 10;
-        for (int i = x; i <= y; i++) {
-            for (int j = x; j <= i; j++) {
-                if (j == i) {
-                    System.out.println(i);
-                }
-                if (i % j == 0) {
-                    break;
-                }
+        int maxRange = 10;
+        for(int i = 0; i <= maxRange; i++) {
+            if(isPrimeNumber(i)) {
+                System.out.println(i);
             }
         }
+    }
+    private static boolean isPrimeNumber(int number) {
+        if(number < 2) {
+            return false;
+        }
+        for(int i = 2; i <=number/2; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
