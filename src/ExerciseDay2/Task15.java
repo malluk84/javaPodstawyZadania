aaf554be8c5dc190fd1257e4ca573136c54c97d7
Lukasz Malachowski <malachowski.lukasz@gmail.com>
2021-09-12 16:11:46 +0200

    Day 2 Task17

--- src/ExerciseDay2/Task15.java
+++ src/ExerciseDay2/Task15.java
@@ -3,22 +3,27 @@ package ExerciseDay2;
 public class Task15 {
     public static void main(String[] args) {
         String[] firstArray = {"Ala", "ma", "kota"};
-        String[] secondArray = {"Ala", "ma", "kotaa"};
+        String[] secondArray = {"Ala", "ma", "kota"};
         checkArray(firstArray, secondArray);
+        System.out.println(checkArray(firstArray, secondArray));
+
     }
-    private static void checkArray(String[] firstArray, String[] secondArryay) {
-        if (firstArray.length != secondArryay.length) {
-            System.out.println("Arrays are diffrent");
-        } else {
-            boolean ifArraydiffrent = false;
+    private static boolean checkArray(String[] firstArray, String[] secondArryay) {
+        if (firstArray.length == secondArryay.length) {
             for (int i = 0; i < firstArray.length; i++) {
                 if (!firstArray[i].equals(secondArryay[i])) {
-                    ifArraydiffrent = true;
-                    break;
+                    return false;
                 }
             }
-            System.out.println(!ifArraydiffrent);
+            return true;
+            /*System.out.println("Arrays are diffrent");
+            return false;*/
         }
+        /*else {
+            boolean ifArraydiffrent = false;
+            System.out.println(!ifArraydiffrent);
+        }*/
+        return false;
     }
 }
 
---
+++ src/ExerciseDay2/Task17.java
@@ -0,0 +1,35 @@
+package ExerciseDay2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task17 {
+    /*public static void main(String[] args) {
+        int[] inputs = getAndCreateArrayFromUser();
+        Arrays.stream(sortArray(inputs)).forEach(System.out::println);
+
+    private static int[] sortArray(int[] inputs) {
+            for (int i = 0; i < inputs.length; i++) {
+                for (int j = 0; j < inputs; j++) {
+                    if (inputs[j]> inputs[j+1]) {
+                        int tmp = inputs[j];
+                        inputs[j] = inputs[j+1];
+                        inputs[j+1] = tmp;
+                    }
+                }
+            }
+        }
+
+    private static int[] getAndCreateArrayFromUser() {
+            Scanner scanner = new Scanner(System.in);
+            int[] inputs = new int[10];
+            int counter = 0;
+            while(counter < 10) {
+                System.out.println("Please enter digits: ");
+                inputs[counter] = scanner.nextInt();
+                counter++;
+            }
+            return inputs;
+        }
+    }*/
+}
