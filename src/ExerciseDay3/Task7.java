d6c804dd9545617e1405226a33172aabc7bbed67
Lukasz Malachowski <malachowski.lukasz@gmail.com>
2021-09-18 10:33:44 +0200

    Day 3 Task7

--- src/ExerciseDay3/Task7.java
+++ src/ExerciseDay3/Task7.java
@@ -1,2 +1,27 @@
-package ExerciseDay3;public class Task7 {
+package ExerciseDay3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+
+    String correctIban = "PL10155000997603123456789123";
+    String inCorrectIban = "1234";
+
+        System.out.println(correctIban + " " + isCorrectIbanFormat(correctIban));
+        System.out.println(inCorrectIban + " " + isCorrectIbanFormat(inCorrectIban));
+
+}
+    private static boolean isCorrectIbanFormat(String iban) {
+        // + jede lub wiele znaków
+        //* zero lub wiele znaków
+        //? zero lub jeden znak
+        String regex = "PL[0-9]{26}";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(iban).matches();
+       /* Matcher matcher = pattern.matcher(iban);
+        return matcher.matches();*/
+    }
 }
