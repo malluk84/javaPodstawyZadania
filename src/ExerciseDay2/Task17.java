086b4a80e427f892c74b553412e69ab972293b98
Lukasz Malachowski <malachowski.lukasz@gmail.com>
2021-09-18 09:41:16 +0200

    Day 2 Task18

--- src/ExerciseDay2/Task17.java
+++ src/ExerciseDay2/Task17.java
@@ -4,32 +4,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task17 {
-    /*public static void main(String[] args) {
+
+    public static void main(String[] args) {
         int[] inputs = getAndCreateArrayFromUser();
         Arrays.stream(sortArray(inputs)).forEach(System.out::println);
+    }
 
     private static int[] sortArray(int[] inputs) {
-            for (int i = 0; i < inputs.length; i++) {
-                for (int j = 0; j < inputs; j++) {
-                    if (inputs[j]> inputs[j+1]) {
-                        int tmp = inputs[j];
-                        inputs[j] = inputs[j+1];
-                        inputs[j+1] = tmp;
-                    }
+        for (int i = 0; i < inputs.length - 1; i++) {
+            for (int j = 0; j < inputs.length - 1; j++) {
+                if (inputs[j] > inputs[j + 1]) {
+                    int tmp = inputs[j];
+                    inputs[j] = inputs[j + 1];
+                    inputs[j + 1] = tmp;
                 }
             }
         }
+        return inputs;
+    }
 
     private static int[] getAndCreateArrayFromUser() {
-            Scanner scanner = new Scanner(System.in);
-            int[] inputs = new int[10];
-            int counter = 0;
-            while(counter < 10) {
-                System.out.println("Please enter digits: ");
-                inputs[counter] = scanner.nextInt();
-                counter++;
-            }
-            return inputs;
+        Scanner scanner = new Scanner(System.in);
+        int[] inputs = new int[10];
+        int counter = 0;
+        while (counter < 10) {
+            System.out.print("Podaj liczbę: ");
+            inputs[counter] = scanner.nextInt();
+            counter++;
         }
-    }*/
+        return inputs;
+    }
 }
